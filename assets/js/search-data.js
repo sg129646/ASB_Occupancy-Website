b5b37c1e0e5b148d6b82fad5ed2433c38d292926
sg129646 <scigwe@gmail.com>
2026-04-02 19:08:02 +0000

    Deploying to gh-pages from @ sg129646/ASB_Occupancy-Website@f6ccc0e24d0d8c88e6355edb05013dbd94d305d2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/ASB_Occupancy-Website/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
+        },{id: "nav-code-repositories",
+          title: "code repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
