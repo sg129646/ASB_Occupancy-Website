bf80841a4eae9fae82dbfeb6f517f0db5a10ecbc
sg129646 <scigwe@gmail.com>
2026-04-02 21:44:08 +0000

    Deploying to gh-pages from @ sg129646/ASB_Occupancy-Website@8f92d204816a027bc4749395e3c2a0516cebbab9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -423,8 +423,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-project-plan",
+          title: 'Project Plan',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/ASB_Occupancy-Website/projects/1_project/";
