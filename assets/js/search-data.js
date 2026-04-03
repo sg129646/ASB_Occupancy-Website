558a39c0f4ecefde797b12bcb1707da86c3a44de
sg129646 <scigwe@gmail.com>
2026-04-03 00:20:13 +0000

    Deploying to gh-pages from @ sg129646/ASB_Occupancy-Website@ace08b7c38331a19b0a412aab602e80e74ea5467 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -443,9 +443,9 @@ ninja.data = [{
           description: "Space to discuss anything else with the project",
           section: "Projects",handler: () => {
               window.location.href = "/ASB_Occupancy-Website/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-final-document-uploads",
+          title: 'Final Document Uploads',
+          description: "Place to provide slides, reports, etc.",
           section: "Projects",handler: () => {
               window.location.href = "/ASB_Occupancy-Website/projects/5_project/";
             },},{id: "projects-project-6",
