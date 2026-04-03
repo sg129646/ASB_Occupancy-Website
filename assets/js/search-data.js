a878f7a526e88ffafde1c74c8e1bd0334dd4cc9e
sg129646 <scigwe@gmail.com>
2026-04-03 11:34:17 +0000

    Deploying to gh-pages from @ sg129646/ASB_Occupancy-Website@906e71d9537f61ddc36a684fea414faa07e699fb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -497,7 +497,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%67%6F%64%66%72%65%65@%61%6E%64%72%65%77.%63%6D%75.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -518,14 +518,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=FOt69Y8AAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://github.com/sg129646/CMU-ASB_Occupancy-Prediction-Group", "_blank");
         },
       },{
       id: 'light-theme',
